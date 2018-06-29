2522bbb6898eeb230d3e6d46791b01fed3b8bc44
Teresa <mtnietotcpsi@gmail.com>
2018-06-29 22:00:00 +0200

    añadidos todos los canales

--- scripts/05.sh
+++ scripts/05.sh
@@ -9,3 +9,9 @@ CORE_PEER_TLS_ENABLED=true
 
 peer channel join -b $CHANNEL_NAME.block  >&log.txt
 cat log.txt
+
+CORE_PEER_ADDRESS=peer1.dac.upc.edu:7051
+
+echo "Join de DAc a Canal0"
+peer channel join -b canal0.block  >&log.txt
+cat log.txt
